3d7c766a866312e63cc1d9c75c71df92c7e0a19f
Veli Agiragac <vagiragac14@gmail.com>
2018-02-22 16:04:22 +0300

    ilk commit

---
+++ 14253001HW3/Dikdörtgen.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14253001HW3
+{
+    class Dikdörtgen
+    {
+        int x = 0;//x yönü
+        int y = 0;//y yönü
+        int a = 0;//eni
+        int b = 0;//boyu
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("hatalı giriş yaptınız:::");
+                    Console.WriteLine("\n");
+                }
+
+                else
+                    a = value;
+            }
+        }//en  kontrolü
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("hatalı giriş yaptınız:::");
+                    Console.WriteLine("\n");
+                }
+
+                else
+                    b = value;
+            }
+        }//boy kontrolü
+
+        public Dikdörtgen(int x, int y, int a, int b)
+        {
+            this.x = x;
+            this.y = y;
+            A = a;
+            B = b;
+        }
+       
+        
+        public void hareket(int x,int y)//x ve y hareketlerinin kontrolleri
+        {
+            Console.WriteLine("X VE Y İÇİN HAREKET KONTROLLERİ");
+            Console.WriteLine("-------------------------------");
+            x +=  this.x;
+            y +=  this.y;
+            Console.WriteLine("x'in hareketi=" +x +"\n"+"y'nin hareketi="+y);
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 1; j <= y; j++)
+                {
+                    Console.Write("*");
+                }
+               
+                Console.WriteLine();
+            }
+                Console.WriteLine("\n");
+
+
+        }
+       
+        public void DikdortgenCevre()
+        {
+            Console.WriteLine("DİKDÖRTGEN İÇİN ÇEVRE HESAPLAMA");
+            Console.WriteLine("-------------------------------");
+            int cevre = 0;
+            if(a<=0||b<=0)
+                Console.WriteLine("dikdörtgenin çevresi olmaz:");
+            else
+                 cevre = 2*(a+b);
+            Console.WriteLine("dikdörgenin çevresi="+cevre);
+            Console.WriteLine("\n");
+        }
+        
+        public void DikdörtgenAlan()
+        {
+            Console.WriteLine("DİKDÖRTGEN İÇİN ALAN HESAPLAMA");
+            Console.WriteLine("------------------------------");
+            int alan = 0;
+            if (a <= 0 || b <= 0)
+                Console.WriteLine("dikdörtgenin alanı olmaz:");
+            else
+                alan = a*b;
+            Console.WriteLine("dikdörtgenin alanı="+alan);
+            
+            }
+        }
+    
+}
---
+++ 14253001HW3/DikdörtgenTest.cs
@@ -0,0 +1,26 @@
+﻿//VELİ AĞIRAĞAÇ 
+//14253001
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14253001HW3
+{
+    class DikdörtgenTest
+    {
+        static void Main(string[] args)
+        {
+            Dikdörtgen[] array = new Dikdörtgen[9];
+            array[1] = new Dikdörtgen(1, 1,1,1);
+            array[1].hareket(4,5);
+            array[1].DikdortgenCevre();
+            array[1].DikdörtgenAlan();
+           
+          
+           
+            
+        }
+    }
+}
